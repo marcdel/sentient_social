dc9f1c09b8260f58c5d35df41a596dae715cfd7f
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-03-25 16:14:13 -0400

    Adds jquery and bootstrap and some demo layout.

--- apps/sentient_social_web/lib/sentient_social_web/controllers/page_controller.ex
+++ apps/sentient_social_web/lib/sentient_social_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule SentientSocialWeb.PageController do
   use SentientSocialWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    # |> put_flash(:info, "This is a warning")
+    # |> put_flash(:error, "This is an error")
+    |> render("index.html")
   end
 end
