1dedab4d636596e939bae4876ca380dce9d77081
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-04-14 23:16:56 -0400

    Add method to find tweets based on the current user's keywords

---
+++ apps/sentient_social/lib/sentient_social/twitter/engagement.ex
@@ -0,0 +1,22 @@
+defmodule SentientSocial.Twitter.Engagement do
+  @moduledoc """
+  Finds tweets matching the specified user's keywords
+  """
+
+  alias SentientSocial.Accounts
+  alias SentientSocial.Accounts.User
+
+  @twitter_client Application.get_env(:sentient_social, :twitter_client)
+
+  @doc """
+  Finds tweets for the given user's keywords
+  """
+  @spec find_tweets(%User{}) :: [%ExTwitter.Model.Tweet{}]
+  def find_tweets(user) do
+    user
+    |> Accounts.list_keywords()
+    |> Enum.flat_map(fn keyword ->
+      @twitter_client.search(keyword.text, count: 1)
+    end)
+  end
+end
