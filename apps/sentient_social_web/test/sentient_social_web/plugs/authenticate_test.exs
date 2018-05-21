593a41696348142b56dcc430d07d2687d5b70c0e
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-05-21 19:47:14 -0400

    100% test coverage 🎉

--- apps/sentient_social_web/test/sentient_social_web/plugs/authenticate_test.exs
+++ apps/sentient_social_web/test/sentient_social_web/plugs/authenticate_test.exs
@@ -2,6 +2,7 @@ defmodule SentientSocialWeb.AuthenticateTest do
   @moduledoc """
   """
   use SentientSocialWeb.ConnCase, async: true
+  alias SentientSocialWeb.Plug.Authenticate
 
   test "user is redirected when current_user is not set", %{conn: conn} do
     conn = get(conn, dashboard_path(conn, :index))
@@ -15,5 +16,9 @@ defmodule SentientSocialWeb.AuthenticateTest do
       |> get(dashboard_path(conn, :index))
 
     assert conn.status != 302
+  end
+
+  test "init takes options but call doesn't use them" do
+    assert Authenticate.init(%{}) == %{}
   end
 end
